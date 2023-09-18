4508aa8b2b48c9f3dcec038fc2e64e147f03a91a
Allen Zheng <volcaronitee8533@gmail.com>
2023-09-18 17:08:52 -0400

    fixed kick announce and infinite transfer

--- features/general/AutoTransfer.js
+++ features/general/AutoTransfer.js
@@ -24,21 +24,30 @@ registerWhen(register("chat", (player1, player2) => {
 /**
  * Auto transfer if in lobby.
  */
+let transferred = false;
 registerWhen(register("chat", (player1, player2) => {
-    if (getWorld() !== undefined) return;
+    if (getWorld() !== undefined && transferred === false) return;
     const name1 = getPlayerName(player1).toLowerCase();
     const name2 = getPlayerName(player2).toLowerCase();
 
     if (name1 === Player.getName().toLowerCase()) delay(() => ChatLib.command("p transfer " + name2), 500);
+    transferred = true;
 }).setCriteria("The party was transferred to ${player1} by ${player2}"), () => settings.autoTransfer === 2);
-
+registerWhen(register("worldLoad", () => { transferred = false }), () => settings.autoTransfer === 2);
 registerWhen(register("chat", () => {
     if (getIsLeader() === false) return;
     const party = Array.from(getParty());
     delay(() => ChatLib.command(`p transfer ${party[Math.floor(Math.random() * party.length)]}`), 500);
 }).setCriteria("Oops! You are not on SkyBlock so we couldn't warp you!"), () => settings.autoTransfer === 2);
 
+/**
+ * Announce to party when kicked
+ */
 registerWhen(register("chat", () => {
     if (getInParty() === false) return;
     delay(() => ChatLib.command(`pc ${settings.kickAnnounce}`), 1000);
 }).setCriteria("Oops! You are not on SkyBlock so we couldn't warp you!"), () => settings.kickAnnounce !== "");
+registerWhen(register("chat", () => {
+    if (getInParty() === false) return;
+    delay(() => ChatLib.command(`pc ${settings.kickAnnounce}`), 1000);
+}).setCriteria("You were kicked while joining that server!"), () => settings.kickAnnounce !== "");
