bc9340ba3af834ca4b0bd9fd22f3e52e5ad8c532
Allen Zheng <zhenga8533@gmail.com>
2024-01-02 17:33:00 -0500

    networth formatting

--- features/economy/Networth.js
+++ features/economy/Networth.js
@@ -1,25 +1,42 @@
 import request from "../../../requestV2";
-import { AQUA, DARK_AQUA, DARK_GRAY, DARK_RED, GRAY, GREEN, LOGO, RED, YELLOW } from "../../utils/constants";
-import { decode, formatNumber } from "../../utils/functions";
+import { AQUA, DARK_AQUA, DARK_GRAY, DARK_GREEN, DARK_RED, GRAY, GREEN, LOGO, RED, YELLOW } from "../../utils/constants";
+import { convertToTitleCase, decode, formatNumber } from "../../utils/functions";
 import { getItemValue } from "./ItemPrice";
 
 
 function getInvValue(inv, type) {
+    // Check if API is on
     if (inv === undefined) {
-        ChatLib.chat(` - ${RED + type} API is turned off!`);
+        ChatLib.chat(` ${DARK_GRAY}- ${RED + type} API is turned off!`);
         return 0;
     }
 
+    // Decode inventory NBT
     let items = decode(inv);
     let total = 0;
 
+    // Loop through inventory data
     for (let i = 0; i < items.func_74745_c(); i++) {
         let nbt = new NBTTagCompound(items.func_150305_b(i));
         total += getItemValue(nbt, false);
     }
 
-    ChatLib.chat(` ${DARK_GRAY}- ${AQUA + type} Value: ${GREEN + formatNumber(total)}`);
-    return total;
+    // Return value and message
+    const color = total === 0 ? RED : GREEN;
+    return [total, ` ${DARK_GRAY}- ${AQUA + type} Value: ${color + formatNumber(total)}\n`];
+}
+
+function setInv(name, values) {
+    let value = 0;
+    let msg = `${DARK_AQUA + name} Value:\n`;
+    for (let i = 0; i < values.length; i++) {
+        value += values[i][0];
+        msg += values[i][1];
+    }
+    new TextComponent(` ${DARK_GRAY}- ${AQUA + name} Value: ${GREEN + formatNumber(value)}`).setHoverValue(msg.trim()).chat();
+    values.length = 0;
+
+    return value;
 }
 
 
@@ -68,31 +85,42 @@ export function getNetworth(username, fruit) {
             Object.keys(data).forEach(key => print(key));
 
             // Otherwise calculate networth for inputted profile
-            ChatLib.chat(`\n${LOGO + RED}Calculating networth for ${username} on ${profile.cute_name}...\n`);
+            ChatLib.chat(`\n${LOGO + DARK_AQUA}${username}'s ${profile.cute_name} Networth:\n`);
             let total = 0;
+            let invValues = [];
 
-            // Inventory value:
-            total += getInvValue(data.inv_contents?.data, "Inventory");
+            // Inventory value
+            invValues.push(getInvValue(data.inv_contents?.data, "Inventory"));
+            invValues.push(getInvValue(data.inv_armor?.data, "Armor"));
+            invValues.push(getInvValue(data.equipment_contents?.data, "Equipment"));
+            total += setInv("Player's", invValues);
 
-            // Armor value:
-            total += getInvValue(data.inv_armor?.data, "Armor");
-            total += getInvValue(data.equipment_contents?.data, "Equipment");
-            total += getInvValue(data.wardrobe_equipped_slot?.data, "Wardrobe");
+            // Storage value
+            invValues.push(getInvValue(data.wardrobe_contents?.data, "Wardrobe"));
+            invValues.push(getInvValue(data.ender_chest_contents?.data, "Ender Chest"));
+            invValues.push(getInvValue(data.personal_vault_contents?.data, "Vault"));
+            total += setInv("Storage's", invValues);
 
-            // Storage value:
-            total += getInvValue(data.ender_chest_contents?.data, "Ender Chest");
-            total += getInvValue(data.bag_contents?.data, "Storage");
-            total += getInvValue(data.personal_vault_contents?.data, "Vault");
+            // Backpack values
+            const backpacks = data.backpack_contents;
+            const packs = Object.keys(backpacks).length;
 
-            // Sacks value:
+            for (let i = 0; i < packs; i++) {
+                let backpack = backpacks[i.toString()];
+                invValues.push(getInvValue(backpack?.data, `Backpack ${i + 1}`));
+            }
 
-            /*
-                backpack_icons
-                personal_vault_contents
-                sacks_counts
-                wardrobe_contents
-            */
-           ChatLib.chat(` - Total Networth: ${formatNumber(total)}`);
+            invValues.sort((a, b) => b[0] - a[0]);
+            total += setInv("Backpacks'", invValues);
+
+            // Bag values
+            const bags = data.bag_contents;
+            Object.keys(bags).forEach(bag => invValues.push(getInvValue(bags[bag]?.data, convertToTitleCase(bag))));
+            total += setInv("Bags'", invValues);
+
+            // Total
+           ChatLib.chat(`${DARK_GRAY}Hover over values to see breakdown.`);
+           ChatLib.chat(`\n${LOGO + DARK_AQUA}Total Networth: ${DARK_GREEN + formatNumber(total)}`);
         }).catch((err) => {
             // If there is an error, display the error message in the Minecraft chat.
             ChatLib.chat(`${LOGO + DARK_RED + err.cause ?? err}`);
