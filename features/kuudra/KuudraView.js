cca28a6fc3c7fb8a8dc9bab9667e81b7cbc00b86
Allen Zheng <zhenga8533@gmail.com>
2024-02-05 13:29:35 -0500

    added more '/kv' details

--- features/kuudra/KuudraView.js
+++ features/kuudra/KuudraView.js
@@ -13,7 +13,7 @@ import { decode } from "../../utils/functions/misc";
  * @param {Array[]} terror - Tracked terror armor pieces [tier, stars, message]
  * @returns 
  */
-function containsGoods(inv, type, aurora, terror) {
+function containsGoods(inv, type, aurora, terror, dominance, lifeline) {
     if (inv === undefined) {
         ChatLib.chat(`${DARK_GRAY}- ${RED + type} API is OFF!`);
         return;
@@ -23,6 +23,7 @@ function containsGoods(inv, type, aurora, terror) {
     const GOODS = new Set(["NECRON_BLADE", "HYPERION", "VALKYRIE", "ASTRAEA", "SCYLLA", "TERMINATOR", "RAGNAROCK_AXE"]);
     const TIERS = ["HOT", "BURNING", "FIERY", "INFERNAL"];
     const PIECE = ["HELMET", "CHESTPLATE", "LEGGINGS", "BOOTS"];
+    const EQUIP = ["NECKLACE", "CLOAK", "BELT", "GAUNTLET", "GLOVES"]
     
 
     // Decode inventory NBT
@@ -33,8 +34,10 @@ function containsGoods(inv, type, aurora, terror) {
         // Get item data
         let nbt = new NBTTagCompound(items.func_150305_b(i));
         let tag = nbt.getCompoundTag("tag");
-        let attributes = tag.getCompoundTag("ExtraAttributes");
-        let id = attributes.getString("id");
+        if (tag.hasNoTags()) continue;
+        let extraAttributes = tag.getCompoundTag("ExtraAttributes");
+        let attributes = extraAttributes.getCompoundTag("attributes").toObject();
+        let id = extraAttributes.getString("id");
         let args = id.split('_');
         let display = tag.getCompoundTag("display");
         let name = display.getString("Name");
@@ -42,19 +45,48 @@ function containsGoods(inv, type, aurora, terror) {
         // Check if item is a good :)
         if (GOODS.has(id)) {
             let data = name;
-            let lore = tag.getCompoundTag("display").toObject()["Lore"];
+            let lore = display.toObject()["Lore"];
             lore.forEach(line => data += `\n${line}`);
             new TextComponent(`${DARK_GRAY}- ${name}`).setHoverValue(data).chat();
         } else if (args[1] === "AURORA" || args[1] === "TERROR") {
             let type = args[1] === "AURORA" ? aurora : terror;
             let piece = type[PIECE.indexOf(args[2])];
             let tier = TIERS.indexOf(args[0]);
-            let level = attributes.getInteger("upgrade_level");
+            let level = extraAttributes.getInteger("upgrade_level");
             if (tier > piece[0] || (tier === piece[0] && level > piece[1])) {
                 piece[0] = tier;
                 piece[1] = level;
-                piece[2] = name;
+
+                // Get attribute and add to name
+                let attributeTitle = Object.entries(attributes)
+                    .map(([key, value]) => {
+                        const abbreviation = key.split('_').map(word => word.charAt(0).toUpperCase()).join('');
+                        return abbreviation + value;
+                    }).join(', ');
+                piece[2] = `${GRAY}[${attributeTitle}] ${name}`;
             }
+
+            // Update dom/lifeline on terror
+            if (args[1] === "TERROR") {
+                piece[3] = Math.max(piece[3], attributes?.dominance ?? 0);
+                piece[4] = Math.max(piece[3], attributes?.lifeline ?? 0);
+            }
+        } else if (args.length !== 0) {
+            // Check for dominance/lifeline equip
+            let slot = Math.min(Math.max(EQUIP.indexOf(args[0]), EQUIP.indexOf(args[args.length - 1])), 3);
+            if (slot === -1) continue;
+            let dom = attributes?.dominance;
+            let ll = attributes?.lifeline;
+
+            // Update equip
+            function setEquip(equip, type, newLevel) {
+                if (newLevel > equip[0]) {
+                    equip[0] = newLevel;
+                    equip[1] = `${GRAY}[${type + newLevel}] ${name}`;
+                }
+            }
+            if (dom !== undefined) setEquip(dominance[slot], "D", dom);
+            if (ll !== undefined) setEquip(lifeline[slot], "LL", ll);
         }
     }
 }
@@ -91,26 +123,43 @@ register("command", (name) => {
             // Loop through inventory to check gear.
             ChatLib.chat(`${DARK_AQUA + BOLD}Gear:`);
             const inventory = data?.inventory;
+            // Armor pieces [tier, stars, name, dom, ll]
             const aurora = [[-1, -1, `${RED}Headless`], [-1, -1, `${RED}Heartless`], [-1, -1, `${RED}Pantsgrab`], [-1, -1, `${RED}Socksless`]];
-            const terror = [[-1, -1, `${RED}Headless`], [-1, -1, `${RED}Heartless`], [-1, -1, `${RED}Pantsgrab`], [-1, -1, `${RED}Socksless`]];
+            const terror = [[-1, -1, `${RED}Headless`, 0, 0], [-1, -1, `${RED}Heartless`, 0, 0], [-1, -1, `${RED}Pantsgrab`, 0, 0], [-1, -1, `${RED}Socksless`, 0, 0]];
+            // Equip pieces [attribute level, name]
+            const dominance = [[0, `${RED}Neckless`], [0, `${RED}Cloakless`], [0, `${RED}Fatherless`], [0, `${RED}Handless`]];
+            const lifeline = [[0, `${RED}Neckless`], [0, `${RED}Cloakless`], [0, `${RED}Fatherless`], [0, `${RED}Handless`]];
             if (inventory === undefined) ChatLib.chat(`${DARK_GRAY}- ${RED}Inventory API is OFF!`);
             else {
-                containsGoods(inventory?.inv_contents?.data, "Inventory", aurora, terror);
-                containsGoods(inventory?.ender_chest_contents?.data, "Ender Chest", aurora, terror);
-                containsGoods(inventory?.wardrobe_contents?.data, "Wardrobe", aurora, terror);
+                containsGoods(inventory?.inv_contents?.data, "Inventory", aurora, terror, dominance, lifeline);
+                containsGoods(inventory?.inv_armor?.data, "Armor", aurora, terror, dominance, lifeline);
+                containsGoods(inventory?.ender_chest_contents?.data, "Ender Chest", aurora, terror, dominance, lifeline);
+                containsGoods(inventory?.wardrobe_contents?.data, "Wardrobe", aurora, terror, dominance, lifeline);
 
                 // Loop over backpacks
                 const backpacks = inventory.backpack_contents;
                 const packs = backpacks === undefined ? 0 : Object.keys(backpacks).length;
                 for (let i = 0; i < packs; i++) {
-                    containsGoods(backpacks[i.toString()]?.data, "Backpack", aurora, terror);
+                    containsGoods(backpacks[i.toString()]?.data, "Backpack", aurora, terror, dominance, lifeline);
                 }
                 if (packs === 0) ChatLib.chat(`${DARK_GRAY}- ${RED}Backpack API is OFF!`);
 
                 // Chat out Aurora/Terror pieces in one message
                 ChatLib.chat(`${DARK_AQUA + BOLD}Armor:`);
-                new TextComponent(`${DARK_GRAY}- ${AQUA}Aurora Pieces`).setHoverValue(`${DARK_PURPLE}Aurora Pieces\n${aurora.map(inner => inner[2]).join('\n')}`).chat();
-                new TextComponent(`${DARK_GRAY}- ${AQUA}Terror Pieces`).setHoverValue(`${DARK_PURPLE}Terror Pieces\n${terror.map(inner => inner[2]).join('\n')}`).chat();
+                new TextComponent(`${DARK_GRAY}- ${DARK_PURPLE}Aurora Pieces`)
+                    .setHoverValue(`${DARK_PURPLE}Aurora Pieces\n${aurora.map(inner => inner[2]).join('\n')}`).chat();
+                new TextComponent(`${DARK_GRAY}- ${DARK_PURPLE}Terror Pieces`)
+                    .setHoverValue(`${DARK_PURPLE}Terror Pieces\n${terror.map(inner => inner[2]).join('\n')}`).chat();
+
+                // Equip pieces
+                const totalDom = dominance.reduce((sum, innerArray) => sum + (innerArray.length > 0 ? innerArray[0] : 0), 0) +
+                    terror.reduce((sum, innerArray) => sum + (innerArray.length > 0 ? innerArray[3] : 0), 0);
+                const totolLL = lifeline.reduce((sum, innerArray) => sum + (innerArray.length > 0 ? innerArray[0] : 0), 0) +
+                    terror.reduce((sum, innerArray) => sum + (innerArray.length > 0 ? innerArray[4] : 0), 0);;
+                new TextComponent(`${DARK_GRAY}- ${DARK_PURPLE}Dominance Equips ${GRAY}[Total: ${totalDom}]`)
+                    .setHoverValue(`${DARK_PURPLE}Dominance Equips\n${dominance.map(inner => inner[1]).join('\n')}`).chat();
+                new TextComponent(`${DARK_GRAY}- ${DARK_PURPLE}Lifeline Equips ${GRAY}[Total: ${totolLL}]`)
+                    .setHoverValue(`${DARK_PURPLE}Lifeline Equips\n${lifeline.map(inner => inner[1]).join('\n')}`).chat();
             }
             
             // Check for accessory power
