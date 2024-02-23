4f8db09529df523b1133bb150c856cc5f88db2b4
Allen Zheng <zhenga8533@gmail.com>
2024-02-23 00:06:36 -0500

    fixed garden box

--- features/farming/GardenBox.js
+++ features/farming/GardenBox.js
@@ -1,7 +1,7 @@
 import RenderLib from "../../../RenderLib/index.js";
 import settings from "../../utils/settings";
-import { registerWhen } from "../../utils/variables.js";
-import { getWorld } from "../../utils/worlds.js";
+import { registerWhen } from "../../utils/variables";
+import { getWorld } from "../../utils/worlds";
 
 
 /**
@@ -12,4 +12,4 @@ registerWhen(register("renderWorld", () => {
     const z = Math.floor((Player.getZ() + 240) / 96);
 
     RenderLib.drawEspBox(-192 + x * 96, 67, -192 + z * 96, 96, 10, 1, 1, 1, 1, true);
-}), () => settings.gardenBox && getWorld() === "Garden");
+}), () => getWorld() === "Garden" && settings.gardenBox);
