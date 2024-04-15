f0beb59ae1c6efc562000f696b57cfda720feaff
Allen Zheng <zhenga8533@gmail.com>
2024-04-15 14:57:20 -0400

    Added /missingSkins

--- features/economy/MissingSkins.js
+++ features/economy/MissingSkins.js
@@ -1,4 +1,6 @@
-import { GREEN, LOGO, RED } from "../../utils/constants";
+import axios from "../../../axios";
+import { AQUA, DARK_GRAY, GREEN, ITALIC, LOGO, RED } from "../../utils/constants";
+import { decode } from "../../utils/functions/misc";
 import { data } from "../../utils/variables";
 
 
@@ -11,7 +13,10 @@ register("guiOpened", () => {
             for (let j = 1; j < 8; j++) {
                 let skin = items[i * 9 + j];
                 if (skin === null) break;
+
                 let skinID = skin.getNBT().getCompoundTag("tag").getCompoundTag("ExtraAttributes").getString("id");
+                if (skinID.endsWith("RUNE") || skinID.startsWith("DYE") || skinID.endsWith("BARN_SKIN")) continue;
+
                 if (!data.skins.includes(skinID)) data.skins.push(skinID);
             }
         }
@@ -24,7 +29,71 @@ register("command", () => {
         return;
     }
 
-    let missingSkins = "";
+    axios.get(`https://sky.shiiyu.moe/api/v2/profile/${Player.getName()}`).then(response => {
+        let missing = [...data.skins];
 
-    ChatLib.chat(missingSkins || `${LOGO + GREEN}No missing skins!`);
+        function parseSkins(contents) {
+            if (contents === undefined) return;
+
+            let items = decode(contents);
+            for (let i = 0; i < items.func_74745_c(); i++) {
+                let nbt = new NBTTagCompound(items.func_150305_b(i)).getCompoundTag("tag").getCompoundTag("ExtraAttributes");
+
+                let skin = nbt.getString("skin");
+                if (skin) {
+                    let index = missing.indexOf(skin);
+                    if (index !== -1) missing.splice(index, 1);
+                }
+
+                let item = nbt.getString("id");
+                if (item) {
+                    let index = missing.indexOf(item);
+                    if (index !== -1) missing.splice(index, 1);
+                }
+            }
+        }
+
+        const profiles = response.data.profiles;
+        const player = profiles[Object.keys(profiles).find(prof => profiles[prof].current)]?.raw;
+        if (player === undefined) {
+            ChatLib.chat(`${LOGO + RED}Error fetching player profile!`);
+            return;
+        }
+        const inv = player.inventory;
+        if (inv === undefined) {
+            ChatLib.chat(`${LOGO + RED}Player inventory API is turned off!`);
+            return;
+        }
+        
+        // Inventroy values
+        parseSkins(inv.inv_contents?.data);
+        parseSkins(inv.inv_armor?.data);
+        parseSkins(inv.equipment_contents?.data);
+
+        // Storage values
+        parseSkins(inv.wardrobe_contents?.data);
+        parseSkins(inv.ender_chest_contents?.data);
+        parseSkins(inv.personal_vault_contents?.data);
+
+        // Backpack values
+        const backpacks = inv.backpack_contents;
+        const packs = backpacks === undefined ? 0 : Object.keys(backpacks).length;
+        for (let i = 0; i < packs; i++) {
+            let backpack = backpacks[i.toString()];
+            parseSkins(backpack?.data)
+        }
+
+        // Pets values
+        const pets = player.pets_data.pets;
+        pets.forEach(pet => {
+            const skin = "PET_SKIN_" + pet.skin;
+            let index = missing.indexOf(skin);
+            if (index !== -1) missing.splice(index, 1);
+        });
+
+        const miss = missing.join(`\n ${DARK_GRAY}-${AQUA}`);
+        ChatLib.chat(
+`${LOGO + GREEN}\n${DARK_GRAY} -${AQUA + miss}
+${DARK_GRAY + ITALIC}This takes a while between updates, also does not track Taylor, backpack, barn, and rune cosmetics...`);
+    }).catch(err => ChatLib.chat(err));
 }).setName("missingSkins");
