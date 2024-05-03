4f38fbde0f9397f27ad2d90de563d1c673e45320
Allen Zheng <zhenga8533@gmail.com>
2024-05-03 07:46:31 -0400

    changed time formatting

--- utils/functions/format.js
+++ utils/functions/format.js
@@ -17,7 +17,7 @@ export function formatTime(seconds, fixed=0) {
         days > 0 ? `${days}d` : '',
         hours > 0 || days > 0 ? `${hours}hr` : '',
         `${minutes < 10 && (hours > 0 || days > 0) ? '0' : ''}${minutes > 0 || hours > 0 || days > 0 ? minutes + 'm' : ''}`,
-        `${remainingSeconds < 10 && (hours > 0 || minutes > 0 || days > 0) ? '0' : ''}${remainingSeconds.toFixed(hours > 0 || minutes > 0 || days > 0 ? 0 : fixed)}s`
+        days > 0 ? '' : `${remainingSeconds < 10 && (hours > 0 || minutes > 0) ? '0': ''}${remainingSeconds.toFixed(hours > 0 || minutes > 0 ? 0 : fixed)}s`
     ].join('');
 
     return timeString;
