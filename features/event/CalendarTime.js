2001e5fd6c435f9311f12ed7d81ba78a97e8defc
Allen Zheng <zhenga8533@gmail.com>
2024-05-06 10:52:04 -0400

    Fixed calendar flicker

--- features/event/CalendarTime.js
+++ features/event/CalendarTime.js
@@ -4,11 +4,11 @@ import { unformatTime } from "../../utils/functions/format";
 import { registerWhen } from "../../utils/register";
 
 
+let startDate = 0;
 const tooltip = register("preItemRender", (_, __, slot) => {
     const item = Player.getContainer().getItems()[slot.getSlotIndex()];
     if (item === null) return;
     const lore = item.getLore().join('\n').split('\n').slice(1).map(line => line.substring(4));
-    let startDate = 0;
 
     // Calendar day date
     if (item.getName().startsWith("§aDay") && !item.getName().endsWith("]")) {
@@ -25,21 +25,19 @@ const tooltip = register("preItemRender", (_, __, slot) => {
 
         item.setName(`§aDay ${day + 1} §7[${YELLOW + date}, ${time.substring(0, time.length - 3).trim()}§7]`);
     } else {  // Calendar event date
-        for (let i = 0; i < lore.length; i++) {
+        for (let i = 0; i < lore.length + 1; i++) {
             if (lore[i]?.startsWith("§7Starts in:") && !lore[i + 1]?.startsWith("§7Start Date")) {
                 startDate = unformatTime(lore[i].removeFormatting()) * 1_000 + Date.now();
                 const start = new Date(Math.round(startDate / 60_000) * 60_000);
                 const date = start.toLocaleDateString();
                 const time = start.toLocaleTimeString();
                 lore.splice(i + 1, 0, `§7Start Date: ${YELLOW + date}, ${time.substring(0, time.length - 3)}`);
-                break;
             } else if (lore[i]?.startsWith("§7Event lasts for") && !lore[i + 1]?.startsWith("§7End Date")) {
                 const endDate = unformatTime(lore[i].removeFormatting()) * 1_000 + startDate;
                 const end = new Date(Math.round(endDate / 60_000) * 60_000);
                 const date = end.toLocaleDateString();
                 const time = end.toLocaleTimeString();
                 lore.splice(i + 1, 0, `§7End Date: ${YELLOW + date}, ${time.substring(0, time.length - 3)}`);
-                break;
             }
         }
     }
