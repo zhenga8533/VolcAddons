cf76551a94bbc56727e8a40adaf4ee39ef5b11e3
Allen Zheng <zhenga8533@gmail.com>
2024-05-06 11:32:41 -0400

    Fixed tooltip flicker

--- features/container/JyrreTimer.js
+++ features/container/JyrreTimer.js
@@ -6,8 +6,9 @@ import { formatTime } from "../../utils/functions/format";
 /**
  * Adds time NBT tag to Jyrre bottles.
  */
-registerWhen(register("itemTooltip", (_, item) => {
-    if (!item.getName().endsWith("Bottle of Jyrre")) return;
+registerWhen(register("preItemRender", (_, __, ___, gui) => {
+    const item = Player.getContainer().getItems()[gui?.getSlotUnderMouse()?.field_75222_d];
+    if (!item || !item.getName().endsWith("Bottle of Jyrre")) return;
     
     const itemTag = item.getNBT().getCompoundTag("tag");
     const loreTag = itemTag.getCompoundTag("display").getTagMap().get("Lore");
