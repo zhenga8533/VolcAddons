a4fe4d2ca55f79dee7f23e53d054be66a8a51753
Allen Zheng <zhenga8533@gmail.com>
2024-05-09 00:19:02 -0400

    set up button edit and input

--- utils/buttons.js
+++ utils/buttons.js
@@ -7,7 +7,12 @@ data.chestButtons = {};
 const InventoryBasic = Java.type("net.minecraft.inventory.InventoryBasic");
 const GuiInventory = Java.type("net.minecraft.client.gui.inventory.GuiInventory")
 const GuiChest = Java.type("net.minecraft.client.gui.inventory.GuiChest");
+const GuiTextField = Java.type("net.minecraft.client.gui.GuiTextField");
+const commandInput = new GuiTextField(0, Client.getMinecraft().field_71466_p, 10, 10, 176, 16);
+const iconInput = new GuiTextField(0, Client.getMinecraft().field_71466_p, 10, 30, 176, 16);
 
+let invButtons = {};
+let chestButtons = {};
 let buttons = {};
 
 class Button {
@@ -39,6 +44,36 @@ class Button {
     }
 }
 
+let editID;
+let editX = 0;
+let editY = 0;
+
+const inputClick = register("guiMouseClick", (x, y, button, _, event) => {
+    commandInput.func_146192_a(x, y, button);
+    iconInput.func_146192_a(x, y, button);
+    if (commandInput.func_146206_l() || iconInput.func_146206_l()) cancel(event);
+}).unregister();
+
+const inputKey = register("guiKey", (char, keyCode, _, event) => {
+    if (commandInput.func_146206_l()) {
+        commandInput.func_146201_a(char, keyCode);
+        if (keyCode === 28) {  // Enter key
+            invButtons[editID] = new Button(editX, editY, () => {
+                ChatLib.command(commandInput.func_146179_b());
+            }, "dirt")
+        }
+    } else if (iconInput.func_146206_l()) iconInput.func_146201_a(char, keyCode);
+    else return;
+
+    // Cancel all but escape key
+    if (keyCode !== 1) cancel(event);
+}).unregister();
+
+const inputRender = register("guiRender", () => {
+    commandInput.func_146194_f();
+    iconInput.func_146194_f();
+}).unregister();
+
 function createButtons(start, end, interval, typeButtons, category, slots, left, top, dx, dy) {
     for (let i = start; i < end; i += interval) {
         let id = category + i;
@@ -53,7 +88,12 @@ function createButtons(start, end, interval, typeButtons, category, slots, left,
             let x = left + slot.field_75223_e + dx;
             let y = top + slot.field_75221_f + dy;
             buttons[id] = new Button(x, y, () => {
-                ChatLib.chat("E");
+                editID = id;
+                editX = x;
+                editY = y;
+                inputClick.register();
+                inputKey.register();
+                inputRender.register();
             }, "barrier");
         }
     }
@@ -68,6 +108,12 @@ export function editInvButtons() {
         const top = gui.getGuiTop();
         const left = gui.getGuiLeft();
         const slots = gui.field_147002_h;
+
+        // Set input field locations
+        commandInput.field_146209_f = left;
+        commandInput.field_146210_g = top - 80;
+        iconInput.field_146209_f = left;
+        iconInput.field_146210_g = top - 50;
 
         // Set all inv edit buttons
         createButtons(1, 5, 1, data.invButtons, "crafting", slots, left, top, 0, 0);
@@ -99,6 +145,12 @@ export function editChestButtons() {
         const slots = gui.field_147002_h;
         const size = slots.func_75138_a().length;
 
+        // Set input field locations
+        commandInput.field_146209_f = left;
+        commandInput.field_146210_g = top - 80;
+        iconInput.field_146209_f = left;
+        iconInput.field_146210_g = top - 50;
+
         // Set all inv edit buttons
         createButtons(0, 9, 1, data.chestButtons, "top", slots, left, top, 0, -36);
         createButtons(size - 9, size, 1, data.chestButtons, "bottom", slots, left, top, 0, 27);
@@ -111,11 +163,19 @@ const click = register("guiMouseClick", (x, y) => {
     Object.keys(buttons).forEach(key => {
         buttons[key].click(x, y);
     });
+
+    Object.keys(invButtons).forEach(key => {
+        invButtons[key].click(x, y);
+    });
 }).unregister();
 
 const render = register("guiRender", () => {
     Object.keys(buttons).forEach(key => {
         buttons[key].draw();
+    });
+
+    Object.keys(invButtons).forEach(key => {
+        invButtons[key].draw();
     });
 });
 
@@ -124,6 +184,9 @@ const close = register("guiClosed", () => {
     render.unregister();
     click.unregister();
     close.unregister();
+    inputRender.unregister();
+    inputClick.unregister();
+    inputKey.unregister();
 }).unregister();
 
 register("guiOpened", (event) => {
