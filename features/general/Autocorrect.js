8d8fc1dac697510aa9801d96dd80ba4c2901044b
Allen Zheng <zhenga8533@gmail.com>
2024-05-12 23:42:06 -0400

    stop autocomplete parsing for dev :)

--- features/general/Autocorrect.js
+++ features/general/Autocorrect.js
@@ -223,14 +223,16 @@ register("command", () => {
 /**
  * Parse out uncommon commands/words
  */
-data.wordbanks.forEach(wordbank => {
-    Object.keys(wordbank).forEach(word => {
-        wordbank[word] -= 3;
-        if (wordbank[word] <= 0) delete wordbank[word];
+if (Player.getName() !== "Volcaronitee") {
+    data.wordbanks.forEach(wordbank => {
+        Object.keys(wordbank).forEach(word => {
+            wordbank[word] -= 3;
+            if (wordbank[word] <= 0) delete wordbank[word];
+        });
     });
-});
 
-Object.keys(data.commands).forEach(command => {
-    data.commands[command] -= 3;
-    if (data.commands[command] <= 0) delete data.commands[command];
-});
+    Object.keys(data.commands).forEach(command => {
+        data.commands[command] -= 3;
+        if (data.commands[command] <= 0) delete data.commands[command];
+    });
+}
