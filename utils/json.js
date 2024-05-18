19e72e5eab7cf5aa8485b727f65fa3204faecec0
Allen Zheng <zhenga8533@gmail.com>
2024-05-18 19:37:02 -0400

    configured json class

--- utils/json.js
+++ utils/json.js
@@ -1,3 +1,10 @@
+const files = [];
+register("gameUnload", () => {
+    files.forEach(file => {
+        FileLib.write("VolcAddons", "json/" + file, JSON.stringify(this.data, null, 4));
+    });
+});
+
 export class Json {
     /**
      * Create a new persistant single object JSON file.
@@ -5,12 +12,9 @@ export class Json {
      * 
      * @param {String} file - The name of the JSON file
      */
-    constructor(file) {
+    constructor(file, save=true) {
         this.data = JSON.parse(FileLib.read("VolcAddons", "json/" + file));
-
-        register("gameUnload", () => {
-            FileLib.write("VolcAddons", "json/" + file, JSON.stringify(this.data, null, 4));
-        });
+        if (save) files.push(file);
     }
 
     /**
