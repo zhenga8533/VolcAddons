d51e6bf62caa20513ca3ce8f3a2a8a1c8a9e528e
Allen Zheng <zhenga8533@gmail.com>
2024-06-28 20:40:22 -0400

    Fixed enchant value calc

--- features/economy/ItemPrice.js
+++ features/economy/ItemPrice.js
@@ -24,122 +24,7 @@ import { findClosest } from "../../utils/functions/find";
 import { convertToTitleCase, formatNumber } from "../../utils/functions/format";
 import { getAuction, getBazaar } from "./Economy";
 
-/**
- * Variables used to represent item enchantment data.
- * "ENCHANTMENT_NAME": Minimum max craft.
- */
-const MAX_ENCHANTS = {
-  // Weapon Enchantments
-  BANE_OF_ARTHROPODS: 7,
-  CHAMPION: 1,
-  CLEAVE: 6,
-  CRITICAL: 7,
-  CUBISM: 6,
-  DIVINE_GIFT: 1,
-  DRAGON_HUNTER: 1,
-  ENDER_SLAYER: 7,
-  EXECUTE: 6,
-  EXPERIENCE: 5,
-  FIRE_ASPECT: 3,
-  FIRST_STRIKE: 5,
-  GIANT_KILLER: 7,
-  LETHALITY: 6,
-  LIFE_STEAL: 5,
-  LOOTING: 5,
-  LUCK: 7,
-  MANA_STEAL: 1,
-  PROSECUTE: 6,
-  SCAVENGER: 5,
-  SHARPNESS: 7,
-  SMITE: 7,
-  SMOLDERING: 1,
-  SYPHON: 5,
-  TABASCO: 3,
-  THUNDERBOLT: 6,
-  THUNDERBOLT: 7,
-  TITAN_KILLER: 7,
-  TRIPLE_STRIKE: 5,
-  VAMPIRISM: 6,
-  VENOMOUS: 6,
-  VICIOUS: 5,
-  ULTIMATE_CHIMERA: 1,
-  ULTIMATE_COMBO: 1,
-  ULTIMATE_FATAL_TEMPO: 1,
-  ULTIMATE_INFERNO: 1,
-  ULTIMATE_ONE_FOR_ALL: 1,
-  ULTIMATE_SOUL_EATER: 1,
-  ULTIMATE_SWARM: 1,
-  ULTIMATE_JERRY: 1,
-  ULTIMATE_WISE: 1,
-  CHANCE: 5,
-  INFINITE_QUIVER: 6,
-  OVERLOAD: 1,
-  POWER: 7,
-  SNIPE: 4,
-  ULTIMATE_REITERATE: 1,
-  ULTIMATE_REND: 1,
-  // Armor Enchantments
-  BIG_BRAIN: 3,
-  TRANSYLVANIAN: 4,
-  BLAST_PROTECTION: 7,
-  FEROCIOUS_MANA: 5,
-  FIRE_PROTECTION: 7,
-  GROWTH: 7,
-  HARDENED_MANA: 5,
-  HECATOMB: 1,
-  MANA_VAMPIRE: 1,
-  PROJECTILE_PROTECTION: 6,
-  PROTECTION: 7,
-  REJUVENATE: 1,
-  RESPITE: 1,
-  STRONG_MANA: 5,
-  ULTIMATE_BANK: 1,
-  ULTIMATE_HABANERO_TACTICS: 4,
-  ULTIMATE_LAST_STAND: 1,
-  ULTIMATE_LEGION: 1,
-  ULTIMATE_NO_PAIN_NO_GAIN: 1,
-  ULTIMATE_WISDOM: 1,
-  REFLECTION: 1,
-  COUNTER_STRIKE: 5,
-  TRUE_PROTECTION: 1,
-  SMARTY_PANTS: 1,
-  FEATHER_FALLING: 6,
-  SUGAR_RUSH: 1,
-  ICE_COLD: 1,
-  // Tool Enchantments
-  COMPACT: 1,
-  EFFICIENCY: 1,
-  FORTUNE: 4,
-  PRISTINE: 1,
-  CULTIVATING: 1,
-  DEDICATION: 4,
-  DELICATE: 5,
-  HARVESTING: 6,
-  REPLENISH: 1,
-  TURBO_CACTUS: 1,
-  TURBO_CANE: 1,
-  TURBO_CARROT: 1,
-  TURBO_MUSHROOMS: 1,
-  TURBO_POTATO: 1,
-  TURBO_WARTS: 1,
-  TURBO_WHEAT: 1,
-  SUNDER: 1,
-  TURBO_COCO: 1,
-  TURBO_MELON: 1,
-  TURBO_PUMPKIN: 1,
-  EXPERTISE: 1,
-  ULTIMATE_BOBBIN_TIME: 3,
-  ULTIMATE_FLASH: 1,
-  PALEONTOLOGIST: 1,
-  // Equipment Enchantments
-  CAYENNE: 1,
-  GREEN_THUMB: 1,
-  PROSPERITY: 1,
-  QUANTUM: 3,
-  ULTIMATE_THE_ONE: 4,
-  PESTERMINATOR: 1,
-};
-const STACKING_ENCHANTS = new Set(["EXPERTISE", "COMPACT", "CULTIVATING", "CHAMPION", "HECATOMB", "EFFICIENCY"]);
+const STACKING_ENCHANTS = new Set(["EXPERTISE", "COMPACT", "CULTIVATING", "CHAMPION", "HECATOMB", "TOXOPHILITE"]);
 
 /**
  * This function calculates the value of enchantments on an item, taking into account
@@ -151,23 +36,20 @@ const STACKING_ENCHANTS = new Set(["EXPERTISE", "COMPACT", "CULTIVATING", "CHAMP
  * @returns {Number} - The calculated value of the enchantments on the item.
  */
 function getEnchantmentValue(enchantments, bazaar, type) {
-  value = 0;
+  let value = 0;
   Object.entries(enchantments ?? {}).forEach(([enchant, enchantlvl]) => {
     enchant = enchant.toUpperCase();
-    const maxEnchantLevel = MAX_ENCHANTS[enchant];
-    if (!MAX_ENCHANTS.hasOwnProperty(enchant)) return;
 
-    const enchantName = enchant === "EFFICIENCY" ? "SIL_EX" : `ENCHANTMENT_${enchant}_${enchantlvl}`;
-    const enchantKey = enchant === "EFFICIENCY" ? "SIL_EX" : `ENCHANTMENT_${enchant}_${maxEnchantLevel}`;
-    const base = bazaar?.[enchantKey] ?? 0;
-    let multiplier = enchant === "EFFICIENCY" ? enchantlvl - 5 : 1;
-    multiplier = STACKING_ENCHANTS.has(enchant) ? multiplier : 2 ** (enchantlvl - maxEnchantLevel);
-    value += Math.max(
-      0,
-      (base?.[type] ?? 0) * multiplier,
-      bazaar?.[enchantName]?.[type] ?? bazaar?.[enchantKey]?.[type] ?? 0
-    );
+    if (enchant === "EFFICIENCY") {
+      const multipler = 2 ** (enchantlvl - 6);
+      value += multipler > 0 ? bazaar?.["SIL_EX"]?.[type] * multipler : 0;
+      return;
+    }
+
+    if (STACKING_ENCHANTS.has(enchant)) enchantlvl = 1;
+    value += bazaar?.[`ENCHANTMENT_${enchant}_${enchantlvl}`]?.[type] ?? 0;
   });
+
   return value;
 }
 
